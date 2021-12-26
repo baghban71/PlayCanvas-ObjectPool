208fadebf8901f16a3f5a28db018f62200a91532
Mohammad Reza Baghbani <70829913+baghban71@users.noreply.github.com>
2021-12-26 09:09:05 +0330

    Add files via upload

---
+++ objectPool.js
@@ -0,0 +1,42 @@
+var ObjectPool = pc.createScript('objectPool');
+ObjectPool.attributes.add("tags", { type: "string" });
+
+ObjectPool.prototype.initialize = function () {
+    if (!this.isInit)
+        this.init();
+
+};
+ObjectPool.prototype.init = function () {
+    this.isInit = true;
+    this.pooledObjects = [];
+
+    const prefabsTags = this.tags.split(',');
+
+    this.prefabs = [];
+    prefabsTags.forEach(
+        (item) => {
+            this.pooledObjects.push([]);
+            var asst = this.app.assets.findByTag(item);
+            this.prefabs.push(asst[0]);
+        }
+    );
+};
+ObjectPool.prototype.getPooledObject = function (prefabTypeInt) {
+    if (!this.isInit)
+        this.init();
+    for (var i = 0; i < this.pooledObjects[prefabTypeInt].length; i++) {
+
+        if (!this.pooledObjects[prefabTypeInt][i].enabled) {
+            this.pooledObjects[prefabTypeInt][i].enabled = true;
+            return this.pooledObjects[prefabTypeInt][i];
+        }
+
+    }
+
+    var templateAsset = this.app.assets.get(this.prefabs[prefabTypeInt]._id);
+    var instance = templateAsset.resource.instantiate();
+    this.app.root.addChild(instance);
+    this.pooledObjects[prefabTypeInt].push(instance);
+    return instance;
+};
+
